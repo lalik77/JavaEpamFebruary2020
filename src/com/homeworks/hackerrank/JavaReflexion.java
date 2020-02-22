80e1d1d2d40e28a55b6aaed8c3aa28bf4d6b7d42
Alexander Mami <lalik77@inbox.ru>
2020-02-22 17:08:03 +0400

    Solve JavaReflexion

---
+++ src/com/homeworks/hackerrank/JavaReflexion.java
@@ -0,0 +1,51 @@
+package com.homeworks.hackerrank;
+
+
+import java.lang.reflect.Method;
+
+class StudentReflexion {
+
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void anothermethod(){  }
+
+
+}
+
+
+
+public class JavaReflexion {
+
+    public static void main(String[] args) {
+
+        Class student = StudentReflexion.class;
+        StudentReflexion reflexion = new StudentReflexion();
+        //Class student2 = Class.forName("com.homeworks.hackerrank.StudentReflexion"); // error надо выбромить исключение
+        Class student3 = reflexion.getClass(); // вернет объект класса класс через экземпляр класса
+
+        //System.out.println(student); // -> class com.homeworks.hackerrank.StudentReflexion
+        //System.out.println(student3); // -> class com.homeworks.hackerrank.StudentReflexion
+
+        /*Создаем массив типа Method куда засунем декларированные методы в нашем классе*/
+        Method[] methods = student.getDeclaredMethods();
+
+        for ( Method m: methods) {
+            System.out.println(m.getName());
+
+        }
+
+    }
+
+
+}
